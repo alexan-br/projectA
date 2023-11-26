ca85b8bc00da7c41e236f9ac3e332e4af916ca4b
Alexandre <braudalexandre44@gmail.com>
2023-11-26 23:08:26 +0100

    testing img error on staging

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,10 @@
     import productData from '@/cms/queries/productData';
     
     const { data: allProducts, pending: productPending, error:productError } = await useLazyAsyncQuery(productData);
-    
+    const handleImageError = (url) => {
+  console.error('Error loading image:', url);
+  // Handle image loading error as needed
+};
 </script>
 <template>
     <h1>Home</h1>
@@ -11,7 +14,7 @@
         <!-- {{ product }}  -->
         <div v-for="gallery in product.productGallery" :key="product.productGallery.id">
             <!-- {{ gallery.gallery }} -->
-            <img v-for="url in gallery.gallery" :key="gallery.gallery.id" :src="url.url">
+            <img v-for="url in gallery.gallery" :key="url.id" :src="url.url" @error="handleImageError(url)">
                 <!-- {{ url.url }} -->
         </div>
     </div>
