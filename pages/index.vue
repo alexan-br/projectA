e618df6bf9966a7c17aca8c518a6f86c4164f356
Alexandre <braudalexandre44@gmail.com>
2023-12-01 00:00:02 +0100

    last testing pushes

--- pages/index.vue
+++ pages/index.vue
@@ -4,31 +4,48 @@
 
 </script>
 <template v-if="!productPending">
-    <h1>ProjectA</h1>
-    <p>Be different</p>
-    <div v-if="allProducts" v-for="product in allProducts.allProducts" :key="product.id" class="product-list">
-      <NuxtLink :to="`/products/${product.productSlug}`">
-        <div class="product-card">
-          <img v-if="product.productGallery.length > 0" :src="product.productGallery[0].gallery[0].url" :alt="product.productTitle" @error="handleImageError(product.productGallery[0].url)">
-          <h2>{{ product.productTitle }}</h2>
-          <p>{{ product.productPrice }}€</p>
-          <div v-for="brandInfo in product.productBrand" :key="brandInfo.brandName.brandName">
-            <p>
-              {{ brandInfo.brandName.brandName }}
-            </p>
-          </div>
-          <div>
-            <span v-for="colorInfo in product.productColor" :key="colorInfo.color.colorName">
-              <span :style="{ backgroundColor: colorInfo.color.color.cssRgb, padding: '4px', margin: '4px', display: 'inline-block' }"></span>
-            </span>
-          </div>
-        </div>
-      </NuxtLink>
+    <div class="banner">
+      <h1>ProjectA</h1>
+      <p>Be different</p>
     </div>
+    <ul class="product-list">
+      <div v-if="allProducts" v-for="product in allProducts.allProducts" :key="product.id">
+        <NuxtLink :to="`/products/${product.productSlug}`">
+          <div class="product-card">
+            <img v-if="product.productGallery.length > 0" :src="product.productGallery[0].gallery[0].url" :alt="product.productTitle" @error="handleImageError(product.productGallery[0].url)">
+            <h2>{{ product.productTitle }}</h2>
+            <p>{{ product.productPrice }}€</p>
+            <div v-for="brandInfo in product.productBrand" :key="brandInfo.brandName.brandName">
+              <p>
+                {{ brandInfo.brandName.brandName }}
+              </p>
+            </div>
+            <div>
+              <span v-for="colorInfo in product.productColor" :key="colorInfo.color.colorName">
+                <span :style="{ backgroundColor: colorInfo.color.color.cssRgb, padding: '4px', margin: '4px', display: 'inline-block' }"></span>
+              </span>
+            </div>
+          </div>
+        </NuxtLink>
+      </div>
+    </ul>
+    
 </template>
 <style lang="scss">
   a {
     display: block !important;
+  }
+  .banner {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 50px 0;
+    background-color: rgb(7, 7, 7);
+    gap: 10px;
+    p {
+      letter-spacing: 5px;
+    }
   }
   .product-card{
     max-width: 300px;
--- pages/products/[slug].vue
+++ pages/products/[slug].vue
@@ -1,17 +1,59 @@
-<script setup>
-  import productData from '@/cms/queries/productBaseSlug';
-  const route = useRoute();
-  const { data: allProducts, pending: productPending, error: productError } = await useLazyAsyncQuery(productData, {slug:route.params.slug});
-  console.log(allProducts)
-</script>
 <template>
   <div v-if="!productPending && allProducts">
     <div v-for="product in allProducts.allProducts" :key="product.id">
       <h1>{{product.productTitle}}</h1>
       <p>{{ product.productPrice}}€</p>
-      <div v-for="gallery in product.productGallery" :key="product.productGallery.id">
-        <img v-for="url in gallery.gallery" :key="url.id" :src="url.url" @error="handleImageError(url)">
+      <div class="gallery">
+        <div v-for="(gallery, galleryIndex) in product.productGallery" :key="galleryIndex">
+          <img
+            v-for="(url, imgIndex) in gallery.gallery"
+            :key="imgIndex"
+            :src="url.url"
+            @error="handleImageError(url)"
+            :class="{ 'main-image': galleryIndex === selectedGalleryIndex && imgIndex === selectedImageIndex }"
+            @click="changeMainImage(galleryIndex, imgIndex)"
+          />
+        </div>
       </div>
     </div>
   </div>
 </template>
+<script setup>
+import { ref } from 'vue';
+import productData from '@/cms/queries/productBaseSlug';
+
+const route = useRoute();
+const selectedGalleryIndex = ref(0);
+const selectedImageIndex = ref(0);
+
+const { data: allProducts, pending: productPending, error: productError } = await useLazyAsyncQuery(productData, { slug: route.params.slug });
+
+const changeMainImage = (galleryIndex, imgIndex) => {
+  selectedGalleryIndex.value = galleryIndex;
+  selectedImageIndex.value = imgIndex;
+};
+</script>
+<style lang="scss">
+.gallery {
+  overflow: hidden;
+  position: relative;
+  > * {
+    img + img{
+      width: 100px;
+      height: 100px;
+      object-fit: cover;
+    }
+  }
+
+  img {
+    display: block;
+    width: 100%;
+    height: auto;
+    &:first-child {
+      width: 500px;
+      height: 500px;
+      object-fit: cover;
+    }
+  }
+}
+</style>
